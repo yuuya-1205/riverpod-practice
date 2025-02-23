8fdbb34b982688fb0d1d74f180ad90e5080f32cf
ushi <>
2025-02-23 12:30:34 +0900

    feature: create todo_detail_page

--- lib/UI/todo_detail_page.dart
+++ lib/UI/todo_detail_page.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_practice/model/logic/todos.dart';
 
 class TodoDetailPage extends ConsumerWidget {
   const TodoDetailPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final todosAsyncValue =
+        ref.watch(todosNotifierProvider("3S7UyC57mt138137mc5m"));
     return Scaffold(
       appBar: AppBar(),
-      body: const Column(
-        children: [],
+      body: Column(
+        children: [
+          Text(todosAsyncValue.valueOrNull?.first.postName ?? ""),
+          Text(todosAsyncValue.valueOrNull?.first.content ?? ""),
+        ],
       ),
     );
   }
