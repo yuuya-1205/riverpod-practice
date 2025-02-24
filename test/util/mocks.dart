b30ebc9914cb1728f0f17ba5243a2ba4339ee3b1
ushi <>
2025-02-25 07:12:43 +0900

    feature: mockを作成

---
+++ test/util/mocks.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod_practice/repository/todo_repository.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([
+  // ここにRepositoryを追加していく。
+  TodoRepository,
+])
+void main() {}
