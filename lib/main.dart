b9c3b89233a76cb385888dee02c97e98f6d00c02
ushi <>
2025-02-24 15:12:28 +0900

    delete: page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod_practice/UI/synchro_page.dart';
 import 'package:flutter_riverpod_practice/UI/todo_page.dart';
 import 'package:flutter_riverpod_practice/firebase_options.dart';
-import 'package:flutter_riverpod_practice/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
