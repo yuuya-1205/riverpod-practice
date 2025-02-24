d000507ec8c3eb00645a3a363ae9dad45611eb08
ushi <>
2025-02-24 11:10:40 +0900

    feature: add documentId argument

--- lib/UI/todo_detail_page.dart
+++ lib/UI/todo_detail_page.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod_practice/model/logic/todos.dart';
+import 'package:flutter_riverpod_practice/model/logic/todo.dart';
 
 class TodoDetailPage extends ConsumerWidget {
-  const TodoDetailPage({super.key});
+  const TodoDetailPage({
+    super.key,
+    required this.documentId,
+  });
+
+  final String documentId;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final todosAsyncValue =
-        ref.watch(todosNotifierProvider("3S7UyC57mt138137mc5m"));
+    final todoAsyncValue = ref.watch(todoNotifierProvider(documentId));
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text(todosAsyncValue.valueOrNull?.first.postName ?? ""),
-          Text(todosAsyncValue.valueOrNull?.first.content ?? ""),
-        ],
+      appBar: AppBar(
+        title: const Text("投稿詳細"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Text("投稿者名："),
+                      Text(todoAsyncValue.value?.postName ?? ""),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Text("内容："),
+                      Text(todoAsyncValue.value?.content ?? ""),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
